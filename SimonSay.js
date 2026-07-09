6bc61ca77e12d9ac9c6f4f143cece53434eaae9b
Animesh Kanjilal <pranatikanjilal0110@gmail.com>
2026-07-09 10:39:46 +0530

    Added mobile support to Simon Says game

--- SimonSay.js
+++ SimonSay.js
@@ -1,78 +1,98 @@
-let gameseq= [];//step-1
-let userseq= [];//step-1
-let colors= ["red","yellow","green","purple"];//step-2
-let started= false;//step-1
-let level=0;//step-1
-let h2=document.querySelector("h2");//step-2
-document.addEventListener("keypress",function(){
-    if(started==false){
-        console.log("game started");
-        started=true;
-        levelup();//step-2
-    }
-});//step-1
-function gameflash(btn){
-    btn.classList.add("flash");
-    setTimeout(function(){
-        btn.classList.remove("flash");
-    },250);
-}//step-2
+let gameseq = [];
+let userseq = [];
 
-function userflash(btn){
-    btn.classList.add("userflash");
-    setTimeout(function(){
-        btn.classList.remove("userflash");
-    },250);
+let colors = ["red", "yellow", "green", "purple"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+// Start game on keyboard or click/tap
+function startGame() {
+    if (started == false) {
+        console.log("Game Started");
+        started = true;
+        levelup();
+    }
 }
 
-function levelup(){
-    userseq= [];
+document.addEventListener("keypress", startGame);
+document.addEventListener("click", startGame);
+
+function gameflash(btn) {
+    btn.classList.add("flash");
+
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 250);
+}
+
+function userflash(btn) {
+    btn.classList.add("userflash");
+
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 250);
+}
+
+function levelup() {
+    userseq = [];
     level++;
-    h2.innerText=`level ${level}`;
-    // game will choose any random btn
-    let randidx=Math.floor(Math.random()*3);
-    let randcolor=colors[randidx];
-    let randbtn=document.querySelector(`.${randcolor}`);
-    //console.log(randidx);
-    //console.log(randcolor);
-    //console.log(randbtn);
+
+    h2.innerText = `Level ${level}`;
+
+    // Choose a random button
+    let randidx = Math.floor(Math.random() * 4);
+    let randcolor = colors[randidx];
+    let randbtn = document.querySelector(`.${randcolor}`);
 
     gameseq.push(randcolor);
-    gameflash(randbtn);
-}//step-2
+    console.log(gameseq);
 
-function checkAns(idx){
-    if(userseq[idx]===gameseq[idx]){
-        if(userseq.length==gameseq.length){
-            setTimeout(levelup,1000);
+    gameflash(randbtn);
+}
+
+function checkAns(idx) {
+    if (userseq[idx] === gameseq[idx]) {
+        if (userseq.length === gameseq.length) {
+            setTimeout(levelup, 1000);
         }
-    }else{
-        h2.innerHTML=`Game Over!  Your score was <b>${level}</b> <br>Press any key to start.`;
-        document.querySelector("body").style.background="red";
-        setTimeout(function(){
-            document.querySelector("body").style.background="white";
-        },150);
+    } else {
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b><br>Tap/Click anywhere or Press any key to restart.`;
+
+        document.querySelector("body").style.background = "red";
+
+        setTimeout(function () {
+            document.querySelector("body").style.background = "white";
+        }, 150);
+
         reset();
     }
 }
 
-function btnpress(){
-    let btn= this;
+function btnpress() {
+    let btn = this;
+
     userflash(btn);
 
-    usercolor=btn.getAttribute("id");
+    let usercolor = btn.getAttribute("id");
     userseq.push(usercolor);
 
-    checkAns(userseq.length-1);
-}
-let allbtns=document.querySelectorAll(".btn");
-for(btn of allbtns){
-    btn.addEventListener("click",btnpress);
+    console.log(userseq);
+
+    checkAns(userseq.length - 1);
 }
 
-function reset(){
-    started=false;
-    gameseq=[];
-    userseq=[];
-    level=0;
+let allbtns = document.querySelectorAll(".btn");
+
+for (let btn of allbtns) {
+    btn.addEventListener("click", btnpress);
+}
+
+function reset() {
+    started = false;
+    gameseq = [];
+    userseq = [];
+    level = 0;
 }
